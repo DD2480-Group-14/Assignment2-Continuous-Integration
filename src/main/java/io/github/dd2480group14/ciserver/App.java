083b81ad0e313b0946ed831c6c4a423717f3371b
melkerolle <melkertrane@outlook.com>
2026-02-04 23:30:14 +0100

    feat: initialized maven project

---
+++ src/main/java/io/github/dd2480group14/ciserver/App.java
@@ -0,0 +1,10 @@
+package io.github.dd2480group14.ciserver;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
