4a30fb7f83447222ff4a6f65c16a4900e3a58d08
r4d1v <codeberg.purr309@passmail.net>
2026-02-08 15:58:47 +0100

    feat: Implement runCommand()

--- src/test/java/io/github/dd2480group14/ciserver/ContinuousIntegrationServerTest.java
+++ src/test/java/io/github/dd2480group14/ciserver/ContinuousIntegrationServerTest.java
@@ -1,19 +1,44 @@
 package io.github.dd2480group14.ciserver;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for Conitinuous Integration Server.
  */
 public class ContinuousIntegrationServerTest {
+	/**
+	 * The command "echo Testing" should
+	 * give the output "Testing"
+	 * runCommand should therefor return
+	 * "Testing"
+	 */
+	@Test
+	public void runCommandOutput() throws IOException, InterruptedException {
+		ContinuousIntegrationServer continuousIntegrationServer = new ContinuousIntegrationServer();
+		File directory = new File("./");
+		List<String> command = List.of("echo", "Testing");
+		String output = continuousIntegrationServer.runCommand(command, directory);
+		assertEquals("Testing", output.trim());
+	}
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+	/**
+	 * The command "Fakecommand" does usually
+	 * not exist in most Unix OS
+	 * runCommand should therefor throw
+	 * an IOException
+	 */
+	@Test
+	public void runCommandFail() {
+		ContinuousIntegrationServer continuousIntegrationServer = new ContinuousIntegrationServer();
+		File directory = new File("./");
+		List<String> command = List.of("Fakecommand");
+		assertThrows(IOException.class, () -> continuousIntegrationServer.runCommand(command, directory));
+	}
 }
